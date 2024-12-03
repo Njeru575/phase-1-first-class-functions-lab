dd6563f206d59b39bc20d2a981b27fcbab32dc5a
emmanuel <emmanjeru2002@gmail.com>
2024-12-03 14:34:48 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-function returnFirstTwoDrivers(drivers) {
-    return drivers.slice(0, 2);
-}
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
 
-function returnLastTwoDrivers(drivers) {
-    return drivers.slice(-2);
-}
-
-function createFareMultiplier(multiplier) {
-    return function (fare) {
-        return fare * multiplier;
-    };
-}
-
-
-function fareDoubler(fare) {
-    return fare * 2;
-}
-
-
-function fareTripler(fare) {
-    return fare * 3;
-}
-
-
-function selectDifferentDrivers(drivers, func) {
-    return func(drivers);
-}
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, selectingFunction) {
+  return selectingFunction(drivers);
+};
